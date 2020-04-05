28f0c4d5a3cfbfc2807ececdee48c9ce6347ee06
Paco <49491889+PecucoR@users.noreply.github.com>
2020-04-05 20:30:31 +0200

    Update programa.py

--- reto1_el_bibliotecario/programa.py
+++ reto1_el_bibliotecario/programa.py
@@ -1,14 +1,9 @@
-import sys
 from reto1 import *
 
 #PROGRAMA PRINCIPAL
 palabra = input("Introduzca la palabra bibliotecario :): ")
 palabra_ca = [] #Dividiremos la palabra en letras
-for i in palabra:
+for i in palabra: 
     palabra_ca.append(i)
 palabra_ca.append("\n")
 cifrado_pal(palabra_ca)
-sys.exit()
-
-
-
